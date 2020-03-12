aaecfbba5394443055fc4cafd2a646293912be9f
Maheshp2001 <saimaheshp2001@gmail.com>
2020-03-12 15:40:20 +0530

    Updates

---
+++ precache-manifest.fb072a312235cc0802d6f48902e26f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e92307e5d95486d6d0c0f7d63fa2e54",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "b49f76a9bfe5edf709a2",
+    "url": "/project1/static/css/main.2178f2a4.chunk.css"
+  },
+  {
+    "revision": "4589ebc2e82dc7fbea6c",
+    "url": "/project1/static/js/2.77b7d457.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.77b7d457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49f76a9bfe5edf709a2",
+    "url": "/project1/static/js/main.fe2abfe8.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "ba35e94442991d754022de6ecfc94f49",
+    "url": "/project1/static/media/perimg.ba35e944.svg"
+  }
+]);
